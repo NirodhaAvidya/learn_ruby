5994594436baa731bc4a4113573a5f7b2939016f
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-08-24 14:04:44 -0400

    Completed ch.05

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,3 +1,22 @@
 def reverser
-  yield.reverse
+  yield.split.map{|e|e.reverse}.join(" ")
+end
+
+# If you prefer the long version:
+  # def reverser
+  #  word_array = yield.split
+  #  backwards_array = []
+  #  word_array.map do |e|
+  #    backwards_array << e.reverse
+  #  end
+  #  result = backwards_array.join(" ")
+  #  return result
+  # end
+
+def adder(x=1)
+  yield+x
+end
+
+def repeater(x=1)
+  x.times (yield)
 end
