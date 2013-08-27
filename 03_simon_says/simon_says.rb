110ab81a1454d4430ce02c8d3055ba116e3b6e48
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-08-26 22:09:24 -0400

    cleaned up after little_words

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -21,20 +21,14 @@ end
 
 # When I got the part about not capitalizing the "little words", I was a bit thrown.  I immediately knew that the method would have to scan for English articles, conjunctions, and prepositions as well as ignoring the first word of the title.  I know that the exercise doesn't call for creating a library of all English prepositions, etc., but I didn't feel right writing the code to pass just the few required test.
 
-def titleize(title)
-  #title.split.map{|word|word.capitalize}.join(" ") ## This worked up until the ignore "little words".
-
-lil_words = File.read(File.expand_path(File.dirname(__FILE__) << '/little_words.txt')).split(/\n/)
-
-  title = title.split
-  result = []
-  for words in title
-    if lil_words.include?(words)
-      result << words
-    else
-      result << words.capitalize!
-    end
+#titleize.split.map{|word|word.capitalize}.join(" ") ## This worked up until the ignore "little words".
+def titleize(new_book)
+  lil_words = File.read(File.expand_path(File.join(".","assets","little_words.txt"))).split(/\n/)
+  title_words = new_book.split(" ")
+  title_words[0].capitalize!
+  title_words.collect do |word|
+    next if lil_words.include? word
+    word.capitalize!
   end
-  result[0].capitalize!
-  result.join(" ")
+  title_words.join(" ")
 end
