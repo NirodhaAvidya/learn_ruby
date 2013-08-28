8461533d5c94e1073faa61c9696389656878666a
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-08-27 23:40:57 -0400

    lab complete

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -1,3 +1,12 @@
-def measure(x=1)
-  yield
+def measure iter=1
+  total = 0
+  iter.times do
+    start = Time.now
+
+    yield
+
+    end_time = Time.now
+    total += end_time - start
+  end
+  total / iter
 end
