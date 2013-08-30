9b5a1f1600d9d637a7eb29c6dbfd4fc8295d9814
NirodhaAvidya <nirodha.avidya@gmail.com>
2013-08-29 23:50:36 -0400

    lab complete

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,12 +1,35 @@
 class XmlDocument
-  # attr_accessor :name
 
-  # def initialize(name="")
-  #   @name = name
-  # end
+  def initialize(indent = false)
+    @indent = indent
+    @tab = 0
+  end
 
-  def hello(string, *args, &block)
-    return "<hello/>"
+  def method_missing(unk_meth, *args, &block)
+    values = args[0] || {}
+    out = ""
+    out << ("  " * @tab) if @indent
+    out << "<#{unk_meth}"
+    values.each_pair {|k,v| out << " #{k}='#{v}'"}
+
+    if block
+      out << ">"
+      out << "\n" if @indent
+      @tab += 1
+      out << yield
+      @tab -= 1
+      out << ("  " * @tab) if @indent
+      out << "</#{unk_meth}>"
+      out << "\n" if @indent
+      # @indent = true
+
+    else
+      out << "/>"
+      out << "\n" if @indent
+    end
+
+    out
+
   end
 
 end
